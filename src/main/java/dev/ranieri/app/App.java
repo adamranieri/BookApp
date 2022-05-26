555a1eb1ee70687440c75fdc63a4fb953ce65f5d
AdamRanieri <adaman94@gmail.com>
2022-05-26 14:48:36 -0400

    Finished DAO

--- src/main/java/dev/ranieri/app/App.java
+++ src/main/java/dev/ranieri/app/App.java
@@ -9,6 +9,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class App {
 
@@ -37,9 +40,37 @@ public class App {
 //        bookDAO.deleteBookById(2);
 
         // get a book from the database
-        Book book = bookDAO.getBookById(3);
-        book.setReleaseYear(2000); // update a field
-        bookDAO.updateBook(book); // save the update in the database
+//        Book book = bookDAO.getBookById(3);
+//        book.setReleaseYear(2000); // update a field
+//        bookDAO.updateBook(book); // save the update in the database
+
+//        Book mockingbird = new Book(0,"To Kill a Mocking Bird", "Harper Lee", 1965);
+//        bookDAO.createBook(mockingbird);
+
+        List<Book> allBooks = bookDAO.getAllBooks();
+        System.out.println(allBooks);
+
+        //Collections is a utility class for dealing with collections
+        //Collections.reverse(allBooks);// flip the order
+        //Collections.shuffle(allBooks);// randomize the list
+
+        // Sorting lists is among the most common tasks in programming
+        // lambda syntax => a function that is treated like a variable/object in the code
+        Comparator<Book> sortByYear = (b1,b2) ->{
+            if(b1.getReleaseYear()< b2.getReleaseYear()){
+                return  -1;
+            }
+            if(b1.getReleaseYear()> b2.getReleaseYear()){
+                return  1;
+            }
+            return 0;
+        };
+        // you can create as many comparators as you want for as many different ways of sorting as you want
+        // adds a lot more flexiblility than just a comparable
+
+        Collections.sort(allBooks, sortByYear);
+        System.out.println(allBooks);
+
 
     }
 }
